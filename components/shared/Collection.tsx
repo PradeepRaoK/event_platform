d5ccb00baf4975a98b96d98fa21244f2ac1ecdc3
Pradeep Rao K <88186518+PradeepRaoK@users.noreply.github.com>
2024-01-08 22:14:43 +0530

    Refactor Collection component to conditionally render Card component

--- components/shared/Collection.tsx
+++ components/shared/Collection.tsx
@@ -33,10 +33,11 @@ const Collection = ({
               const hidePrice = collectionType === 'My_Tickets';
 
               return (
-                <li key={event._id} className="flex justify-center">
-                  <Card event={event} hasOrderLink={hasOrderLink} hidePrice={hidePrice} />
+                <li key={event?._id} className="flex justify-center">
+                  {event && <Card event={event} hasOrderLink={hasOrderLink} hidePrice={hidePrice} />}
                 </li>
-              )
+              );
+              
             })}
           </ul>
 
